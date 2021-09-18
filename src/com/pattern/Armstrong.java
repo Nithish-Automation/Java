1360901f60e18ed40ed30197414c6a52086ca72e
Nithish <Nithish@LAPTOP-6LDUARIU>
2021-09-18 17:15:28 +0530

    armstrong

---
+++ src/com/pattern/Armstrong.java
@@ -0,0 +1,44 @@
+package com.pattern;
+
+import java.util.Scanner;
+
+public class Armstrong {
+	
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Enter any number:");
+        
+         int originalNum = scanner.nextInt();
+        
+         int num = originalNum ;
+        
+         int reminder;
+        
+         int cubedNum = 0;
+                     
+     while(num >0) {
+        
+         reminder = num % 10;
+        
+         num = num / 10;
+    
+         cubedNum = cubedNum + (reminder* reminder* reminder);
+        
+     }
+    
+     if(cubedNum ==originalNum) {
+        
+        System.out.println("Given number is an Armstrong number");
+        
+     } else {
+        
+        System.out.println("Given number is not an Armstrong number");
+        
+     }
+    
+     scanner.close();
+	}
+
+}
